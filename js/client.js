35a00b36060e01c3ec3f49428698728659188420
sushant17-glitch <80514294+sushant17-glitch@users.noreply.github.com>
2021-08-21 13:54:23 +0530

    Add files via upload

---
+++ js/client.js
@@ -0,0 +1,23 @@
+const socket = io("http://localhost:8000");
+
+
+
+// Get DOM elements in respective Js variables
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp')
+const messageContainer = document.querySelector(".container")
+
+const append=(message,position)=>{
+    const messageElement=document.createElement('div')
+    messageElement.innerText=message;
+    messageElement.classList.add(position)
+    messageContainer.append(messageElement);
+}
+
+const name = prompt("Enter your name to join");
+socket.emit('new-user-joined', name);
+
+socket.on('user-joined',name=>{
+    append('${name} joined the chat','right')
+
+})
